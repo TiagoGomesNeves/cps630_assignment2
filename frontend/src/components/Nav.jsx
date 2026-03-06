ff1121d05516314bbd9600897b61b06af48b4b83
Abdurraheem2431 <Abdurraheem1503@outlook.com>
2026-03-05 20:02:35 -0500

    updated nav with new login button

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,9 +6,16 @@ const Nav = ({ username }) => {
         <header className="main-header">
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li><Link to="/home" state={{  username }}>Home</Link></li>
-                    <li><Link to="/profile" state={{username }}>Profile</Link></li>
-                    <li><Link to="/search" state={{  username }}>Search</Link></li>
+                    <li><Link to="/home" state={{  username }} className="nav-item">
+                    <img src="/images/home.png" alt="Home Icon" className="nav-icon" />
+                    Home</Link></li>
+                    <li><Link to="/profile" state={{username }} className="nav-item">
+                    <img src="/images/person.webp" alt="Person Icon" className="nav-icon" />
+                    Profile</Link></li>
+                    <li><Link to="/search" state={{  username }} className="nav-item">
+                    <img src="/images/search.webp" alt="Search Icon" className="nav-icon" />
+                    Search</Link></li>
+                    <li className="nav-logout"><Link to="/">Sign Out</Link></li>
                 </ul>
             </nav>
         </header>
